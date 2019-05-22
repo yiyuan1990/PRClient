5a31bcefa6cba84cb5a521c872769ccd3a5687c9
SJR <18024550106@163.com>
2019-05-22 15:46:11 +0800

    url更改

--- app/src/main/java/com/zkkc/prclient/PRCConstant.java
+++ app/src/main/java/com/zkkc/prclient/PRCConstant.java
@@ -5,45 +5,47 @@ package com.zkkc.prclient;
  */
 public class PRCConstant {
     //本地环境
-//    public static final String BASE_URL = "http://172.16.1.228:8182/";
+//    public static final String BASE_URL = "http://172.16.1.228:8765/";
 //    public static final String WEBSOCKET_BASE_URL = "ws://172.16.1.228:8762/websocket/";
     //测试环境
-//    public static final String BASE_URL = "http://172.16.1.152:8182/";
+//    public static final String BASE_URL = "http://172.16.1.152:8765/";
 //    public static final String WEBSOCKET_BASE_URL = "ws://172.16.1.152:8762/websocket/";
     //生产环境
-    public static final String BASE_URL = "http://120.79.132.29:8182/";
+    public static final String BASE_URL = "http://120.79.132.29:8765/";
+    public static final String WEB = "api/";
+    public static final String IRSVC = "ir-svc/";
     public static final String WEBSOCKET_BASE_URL = "ws://120.79.132.29:8762/websocket/";
 
     /**
      * 萤石AK  测试AccessToken
      */
-    public static final String ACCESS_TOKEN_URL = "device/token";
+    public static final String ACCESS_TOKEN_URL = WEB + "device/token";
 
     /**
      * 登录
      */
-    public static final String LOGIN = "login";
+    public static final String LOGIN = WEB + "login";
     /**
      * 获取线路设备列表
      */
-    public static final String USER_LINE_DEVICE_LIST = "user/lineDeviceList";
+    public static final String USER_LINE_DEVICE_LIST = WEB + "user/lineDeviceList";
 
     /**
      * 媒体列表
      */
-    public static final String MEDIA_LIST = "media/list";
+    public static final String MEDIA_LIST = WEB + "media/list";
     /**
      * 获取塔数据
      */
-    public static final String TOWER_LIST = "tower/list";
+    public static final String TOWER_LIST = WEB + "tower/list";
     /**
      * 获取图片列表
      */
-    public static final String TOWER_PICDATA = "tower/picData";
+    public static final String TOWER_PICDATA = WEB + "tower/picData";
     /**
      * 获取视频列表
      */
-    public static final String TOWER_VIDEODATA = "tower/videoData";
+    public static final String TOWER_VIDEODATA = WEB + "tower/videoData";
 
 
 }
